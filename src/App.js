71b6f71aa777b681eb59d91db4ca0da5adab8958
gmarvis <sgmarvis@gmail.com>
2023-06-24 15:00:08 +0100

    popup modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Home } from "./pages/home";
+import { Wallet } from "./pages/wallet/wallet";
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="wallet" element={<Wallet />} />
+
     </Routes>
  
   );
